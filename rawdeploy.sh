6f040a579ebe1b04b924dc087e3d2d6e3677b17a
alifeki21 <ali.feki21@gmail.com>
2026-04-22 23:45:24 +0100

    add run commands

--- rawdeploy.sh
+++ rawdeploy.sh
@@ -2,14 +2,19 @@
 set -euo pipefail
 LOG_FILE="./deploy.log"
 RELEASE_DIR="./releases"
-REPO_URL="https://github.com/alifeki21/RawDeploy.git"
+REPO_URL="https://github.com/alifeki21/rawdeploy-target-app.git"
+RELEASE_NAME="$(date '+%Y%m%d_%H%M%S')"
 log() {
     echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" | tee -a "$LOG_FILE"
 }
 deploy() {
     log "Starting deployment..."
-    mkdir "$RELEASE_DIR"/"$(date '%Y%m%d_%H%M%S')"
-    git clone "$REPO_URL"
+    git clone -q "$REPO_URL" "$RELEASE_DIR"/"$RELEASE_NAME"
+    pip install -q -r "$RELEASE_DIR/$RELEASE_NAME/requirements.txt"
+    python3 "$RELEASE_DIR/$RELEASE_NAME/app.py" > "$RELEASE_DIR/$RELEASE_NAME/app.log" 2>&1  &
+    echo "$!" > "$RELEASE_DIR/$RELEASE_NAME/app.pid"
+    log "App started with PID $(cat $RELEASE_DIR/$RELEASE_NAME/app.pid)"
+    
 }
 
 rollback() {

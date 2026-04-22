d6a13291f9c160ec658a997bafa383a74dce1598
alifeki21 <ali.feki21@gmail.com>
2026-04-22 23:12:17 +0100

    sep repos,update script

--- rawdeploy.sh
+++ rawdeploy.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -euo pipefail
 LOG_FILE="./deploy.log"
+RELEASE_DIR="./releases"
+REPO_URL="https://github.com/alifeki21/RawDeploy.git"
 log() {
     echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" | tee -a "$LOG_FILE"
 }
 deploy() {
     log "Starting deployment..."
+    mkdir "$RELEASE_DIR"/"$(date '%Y%m%d_%H%M%S')"
+    git clone "$REPO_URL"
 }
 
 rollback() {
